6eb4616581db7763a7a73607aeac148bb0e738f8
Gerard Eklu <gerard_eklu@homedepot.com>
2019-11-03 12:46:04 -0500

    Started implementing the firebase to acess server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -83,7 +84,26 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue!");
+    // alert("You continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Dinho",
+        address: {
+          street: "street 5",
+          ZipCode: 10100,
+          country: "MaVille",
+        },
+        email: "dinho@gmail.com",
+      },
+      deliveryMethod: "fastest",
+    };
+
+    axios
+      .post("/orders.json", order)
+      .then(res => console.log(res))
+      .catch(error => console.log(error));
   };
 
   render() {
